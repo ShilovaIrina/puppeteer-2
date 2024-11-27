13ffa756ff267cf567711ec20b39c2f89b91ddc1
Irina <irashilowa1997@mail.ru>
2024-11-27 20:01:42 +0300

    Первое и второе задание готовы частично

---
+++ features/step_definitions/search.steps.js
@@ -0,0 +1,47 @@
+const puppeteer = require("puppeteer");
+const chai = require("chai");
+const expect = chai.expect;
+const { Given, When, Then, Before, After } = require("cucumber");
+const { clickElement, findText } = require("../../lib/commands.js");
+
+Before(async function () {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const page = await browser.newPage();
+  this.browser = browser;
+  this.page = page;
+});
+
+After(async function () {
+  if (this.browser) {
+    await this.browser.close();
+  }
+});
+
+Given("пользователь открывает сайт киноафиши", async function () {
+  return await this.page.goto("https://qamid.tmweb.ru/client/index.php");
+});
+
+When("пользователь выбирает день сеанса и время", async function () {
+   await clickElement(this.page, "[data-time-stamp='1732914000']");
+   return await clickElement(this.page, "[data-seance-time-stamp='1732960800']");
+});
+
+Then("пользователь переходит на страницу с выбором места в зале на фильм {string}", async function (string) {
+  const actual = await findText(this.page, ".buying__info-title");
+  const expected = await string;
+  expect(actual).contains(expected);
+});
+
+When('пользователь выбирает фильм и бронирует место', async function () {
+  await clickElement(this.page, "[data-time-stamp='1733000400']");
+  await clickElement(this.page, "[data-seance-id='223']");
+  await clickElement(this.page, "div:nth-child(5) span:nth-child(5)");
+  return await clickElement(this.page, ".acceptin-button");
+});
+
+Then('пользователь видит надпись {string}', async function (string) {
+  const actual = await findText(this.page, ".ticket__check-title");
+  const expected = await string;
+  expect(actual).contains(expected);
+  
+});
---
+++ net.test.js
@@ -0,0 +1,37 @@
+const { clickElement, findText,  } = require("./lib/commands.js");
+
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("https://qamid.tmweb.ru/client/index.php");
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Cinema tests", () => {
+  test("Фильм 'Сталкер' 30.11", async () => {
+    await clickElement(page, "[data-time-stamp='1732914000']");
+    await clickElement(page, "[data-seance-time-stamp='1732960800']");
+    const actual = await findText(page, ".buying__info-title");
+    await expect(actual).toContain("Сталкер(1979)");
+  });
+
+  test("Бронируем билет на 'Ведьмака' в вип зале", async () => { 
+    await clickElement(page, "[data-time-stamp='1733000400']");
+    await clickElement(page, "[data-seance-id='223']");
+    await clickElement(page, "div:nth-child(5) span:nth-child(5)");
+    await clickElement(page, ".acceptin-button");
+    const actual = await findText(page, ".ticket__check-title");
+    await expect(actual).toContain("Вы выбрали билеты:");
+  });
+
+  test("Без выбора места, нельзя забронировать билет", async () => {
+    await clickElement(page, "[data-time-stamp='1733000400']");
+    await clickElement(page, "[data-seance-id='223']");
+    const actual = await findText(page, ".acceptin-button");
+    await expect(actual).toHaveAttribute('disabled');
+  });
+});
